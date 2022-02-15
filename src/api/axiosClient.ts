ef270b0d21f238949e97b96958dda5d84734f3a0
vunguyengitacc <nguyenhoanganhvu.it@gmail.com>
2022-02-15 14:53:44 +0700

    initialize

---
+++ src/api/axiosClient.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { IParams } from "model/Common";
+import queryString from "query-string";
+
+const axiosClient = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_API
+      : `http://localhost:8000/`,
+  headers: {
+    "content-type": "application/json",
+  },
+  paramsSerializer: (params: Partial<IParams>) => queryString.stringify(params),
+});
+
+axiosClient.interceptors.request.use(
+  (config: any) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error.response || error.message)
+);
+
+axiosClient.interceptors.response.use(
+  (response: any) => {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error.response.data.message ?? error.response);
+  }
+);
+
+export default axiosClient;
---
+++ src/model/Common.ts
@@ -0,0 +1,6 @@
+export interface IParams {}
+
+export interface IResponse<T> {
+  status: string;
+  data: T;
+}
